73a59ba982fa678d607dd04b2c9ac2ff78420be8
= <omar112694@gmail.com>
2017-03-25 17:57:45 -0700

    Adding post req

--- startLocalServer.py
+++ startLocalServer.py
@@ -1,6 +1,7 @@
 import http.server
 import re
 import random
+import requests
 
 HOST_NAME = "10.10.10.1"
 PORT_NUMBER = 8000
@@ -20,10 +21,13 @@ class CommandServer(http.server.SimpleHTTPRequestHandler):
     vcb1_pattern = re.compile("vcb1-server")
     tcb1_pattern = re.compile("tcb1-server")
 
-    #routes
+    #incoming routes
     vcb1_handle = "/vcb1-server"
     heartbeat_handle = "/heartbeat"
     valve_handle = "/valves"
+
+    #outgoing routes
+    outgoing_handle = "/outgoing"
 
     def do_GET(s):
         print(s.client_address[0], end="")
@@ -55,6 +59,9 @@ class CommandServer(http.server.SimpleHTTPRequestHandler):
         s.data_string = s.rfile.read(int(s.headers['Content-Length']))
 
         print(s.data_string)
+        if(s.valve_handle in s.path):
+            requests.post("10.10.10.3:8000", s.data_string)
+            print("POST sent to 10.10.10.3")
         s.send_response(200)
         #http.server.SimpleHTTPRequestHandler.do_POST(s)
         return
